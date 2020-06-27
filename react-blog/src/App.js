27761043694cacedbe3686f4a6b3f92a39357203
BrandonDalbec <32801607+BrandonDalbec@users.noreply.github.com>
2020-06-27 01:12:18 -0600

    published blogs page implemented (static)

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Components/Home/Home'
 import CreateBlog from './Components/CreateBlog/CreateBlog'
 import './App.css';
+import ViewBlogs from './Components/ViewBlogs/ViewBlogs';
 
 const App = () => {
   return (
@@ -16,12 +17,16 @@ const App = () => {
             <li class="nav-item">
               <a class="nav-link" href="/create-blog">Create Blog</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/view-blogs">View Blogs</a>
+            </li>
           </ul>
         </div>
       </nav>
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route path="/create-blog" component={CreateBlog} />
+        <Route path="/view-blogs" component={ViewBlogs} />
       </BrowserRouter>
     </div>
   )
---
+++ react-blog/src/Components/ViewBlogs/ViewBlogs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css'
+
+const ViewBlogs = () => {
+    return (
+        <div>
+            <div className='center'>
+                <h1 className='pageHeader'>Published Blogs</h1>
+                <h3>Published blogs will go here</h3>
+            </div>
+        </div>
+    )
+}
+
+export default ViewBlogs;
